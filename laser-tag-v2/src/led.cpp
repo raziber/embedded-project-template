f4d0a4c1523c27bcfbba96a4823f60e7a10e8718
razi berg <razi.berg@gmail.com>
2024-05-14 02:33:47 +0300

    native tests are now working, embedded tests not yet

--- laser-tag-v2/src/led.cpp
+++ laser-tag-v2/src/led.cpp
@@ -3,6 +3,8 @@
 #include <Arduino.h>
 #include "led.h"
 
+#define LED_BUILTIN 2
+
 void setLED(int state){
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, state);
--- laser-tag-v2/test/test_embedded/test_example.cpp
+++ laser-tag-v2/test/test_embedded/test_example.cpp
@@ -2,6 +2,8 @@
 #include <Arduino.h>
 #include "led.h"
 
+#define LED_BUILTIN 2
+
 TEST(setLED, test_if_sets_led_to_high){
     setLED(HIGH);
     ASSERT_EQ(digitalRead(LED_BUILTIN), HIGH);
